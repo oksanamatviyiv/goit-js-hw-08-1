e41597e25e6accbd931733959800b24b7fe8c847
oksanamatviyiv <113504985+oksanamatviyiv@users.noreply.github.com>
2023-11-13 10:02:30 +0100

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+player.on('play', function () {
+console.log('played the video!');
+  const savedTime = localStorage.getItem('videoplayer-current-time');
+  if (savedTime) {
+    player
+      .setCurrentTime(parseFloat(savedTime))
+      .catch(error => console.error(error));
+  }
+});
+
+const saveCurrentTime = throttle(() => {
+  player
+    .getCurrentTime()
+    .then(seconds => {
+      localStorage.setItem('videoplayer-current-time', seconds);
+      console.log(`Current time saved: ${seconds}`);
+    })
+    .catch(error => console.error(error));
+}, 1000);
+
+player.on('pause', saveCurrentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,22 @@
+import throttle from 'lodash.throttle';
 
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('.feedback-form');
+  const formData = { email: '', message: '' };
+
+  const saveAndLogData = () => {
+    formData.email = form.email.value;
+    formData.message = form.message.value;
+    console.log('Form data:', formData);
+  };
+  const throttledSaveAndLogData = throttle(saveAndLogData, 500);
+
+  form.email.addEventListener('input', throttledSaveAndLogData);
+  form.message.addEventListener('input', throttledSaveAndLogData);
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log('Form submitted with data:', formData);
+    localStorage.removeItem('feedback-form-state');
+  });
+});
